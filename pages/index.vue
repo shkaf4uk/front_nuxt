1352f4e8e91bfce7a76e0e4cc19074d57db06a72
Maksym_Shafarenko <shafarenkomaksym@gmail.com>
2022-02-03 12:54:29 +0200

    init

---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <Tutorial />
+</template>
+
+<script>
+  import Tutorial from "../components/Tutorial";
+export default {
+  name: 'IndexPage',
+  components: { Tutorial }
+}
+</script>
